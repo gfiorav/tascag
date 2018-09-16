20c14b5f9312b2d48b5a6d571235767b6fa03c7d
Guido Fioravantti <guido.fioravantti@gmail.com>
2018-09-16 16:31:05 +0000

    Super simple POC

---
+++ index.js
@@ -0,0 +1,8 @@
+const router = require('express')();
+
+router.get('/fuera/*', (request, response) => {
+  const name = request.path.split('/')[2];
+  response.send(`Te has cagao fuera, ${name}`);
+});
+
+router.listen(process.env.PORT, () => {});
